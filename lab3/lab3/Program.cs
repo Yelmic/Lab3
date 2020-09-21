26f77127a46b76a45afe8c02bb189c00f46a727b
Elena <mikhnovetz.elena@yandex.ru>
2020-09-21 15:19:52 +0300

    add new lab

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Phone
+    {
+        static Phone()
+        {
+            Console.WriteLine("Создан новый пользователь");
+        }
+        public readonly int id;//только для чтения
+        public string surname;
+        public string name;
+        public string fathername;
+        public string adress;
+        public int card_num;
+        public int debet;
+        public int credit;
+        public int time;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone A = new Phone();//срабатывает статический конструктор
+            Phone phone = new Phone();
+            Console.ReadKey();
+        }
+    }
+}
